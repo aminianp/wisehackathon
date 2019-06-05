6f0b9b8e07222accfec1ca2f4cce94511b73c227
Pouyan Aminian <poaminia@microsoft.com>
2019-06-05 14:08:49 -0700

    testing the basics

--- application.py
+++ application.py
@@ -1,33 +1,6 @@
-import azure.cosmos.cosmos_client as cosmos_client
-from flask import Flask, jsonify
-
-
-
-
-
+from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
-def get():
-        config = {
-            'ENDPOINT': 'https://hackny2.documents.azure.com:443/',
-            'PRIMARYKEY': 'uDB7OOzZ4QYeDOlVMd4TCRKKyFKM8tc0i53CaVhD4ydDLkrdFcigqwMXNk7POme00MqpBH5sHBStEhv0EgW8Dw==',
-            'DATABASE': 'CosmosDatabase',
-            'CONTAINER': 'CosmosContainer'
-        }
-
-        # Initialize the Cosmos client
-        client = cosmos_client.CosmosClient(url_connection=config['ENDPOINT'], auth={
-                                            'masterKey': config['PRIMARYKEY']})
-
-
-        # Create a database
-
-        result_iterable = client.ReadContainers('dbs/hackny2')
-        for item in iter(result_iterable):
-            return jsonify(item)
-        return result_iterable[0]
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def hello():
+    return "Hello World!"
